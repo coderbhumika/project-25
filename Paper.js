474031bc803b286a749b59c49fdd5e50b376b1b6
coderbhumika <78538246+coderbhumika@users.noreply.github.com>
2021-04-06 17:18:20 +0530

    Add files via upload
    project25

---
+++ Paper.js
@@ -0,0 +1,26 @@
+class Paper {
+    constructor(x, y, radius) {
+      var options = {
+        isStatic: false,
+        'restitution':0.3,
+        'friction':0.5,
+        'density':1.2
+      }
+      this.body = Bodies.circle(x, y, radius, options);
+      this.radius = radius;
+      World.add(world, this.body);
+      this.image=loadImage("sprites/paper.png");
+     
+      }
+      display() {
+        var pos = this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        imageMode(CENTER);
+        fill("white");
+        stroke(0);
+        strokeWeight(4);
+        image(this.image,0,0,50,50);
+        pop();
+    }
+    };
